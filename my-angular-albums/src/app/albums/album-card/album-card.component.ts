2390fe35ea82b88cca524f1077c30f5909f5fe98
Brian Timmons <thetimmons@verizon.net>
2019-08-01 08:19:59 -0400

    Checking in files

--- my-angular-albums/src/app/albums/album-card/album-card.component.ts
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Album } from '../album.model';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Album } from "../album.model";
 
 @Component({
-  selector: 'app-album-card',
-  templateUrl: './album-card.component.html',
-  styleUrls: ['./album-card.component.css']
+  selector: "app-album-card",
+  templateUrl: "./album-card.component.html",
+  styleUrls: ["./album-card.component.css"]
 })
 export class AlbumCardComponent implements OnInit {
-
   @Input()
   album: Album;
+  newPrice: number;
 
-  constructor() { }
+  @Output()
+  albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  showAlbum() {
+    this.albumClicked.emit(this.album);
   }
 
+  showArtist() {
+    console.log("Artist selected: " + this.album.artist);
+
+    if (this.album.onSale) {
+      // Apply 10% discount
+      this.newPrice = this.album.price - (this.album.price * .10);
+      }
+  }
 }
--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from "../album.model";
+import { ALBUMS } from "../albums.data";
 
 @Component({
   selector: 'app-album-list',
@@ -14,51 +15,13 @@ export class AlbumListComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.albumsArray = [
-      {
-        id: 1,
-        artist: "Tremonti",
-        albumName: "Dust",
-        onSale: true,
-        price: 11.99,
-        currency: "USD",
-        year: 2016,
-        releaseDate: "April 29, 2016",
-        recordingLocation: "Studio Barbarosa, Orlando, FL",
-        genre: "Pop/Rock",
-        duration: "43:18:00",
-        url: "https://www.allmusic.com/album/dust-mw0002918360"
-      },
-      {
-        id: 2,
-        artist: "Bon Jovi",
-        albumName: "Slippery When Wet",
-        onSale: false,
-        price: 7,
-        year: 1986,
-        currency: "USD",
-        releaseDate: "August 18, 1986",
-        recordingLocation: "Warehouse, Philadelphia, PA",
-        genre: "Pop/Rock",
-        duration: "43:00",
-        url: "https://www.allmusic.com/album/slippery-when-wet-mw0000652380"
-      },
-      {
-        id: 3,
-        artist: "The Beatles",
-        albumName: "Sgt. Pepper's Lonely Hearts Club Band",
-        onSale: true,
-        currency: "GBP",
-        price: 24,
-        year: 1967,
-        releaseDate: "May 26, 1967",
-        recordingLocation: "",
-        genre: "Pop/Rock",
-        duration: "1:33:43",
-        url: "https://www.allmusic.com/album/sgt-peppers-lonely-hearts-club-band-mw0000649874"
-      }];
+    this.albumsArray = ALBUMS;
 
     console.log(this.albumsArray);
   }
+
+  parentFunctionHandler(album) {
+    alert('Album ' + album.albumName + ' was sent from the album card component');
+}
 
 }
